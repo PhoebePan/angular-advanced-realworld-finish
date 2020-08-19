22cb93e9547942e9560e4ab7883f293aee5929b9
黃升煌 Mike <mike@miniasp.com>
2020-08-19 17:35:56 +0800

    任務 01 步驟 09：建立 CreateComponent，並加入 PostsModule 內

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.component';
 import { PostComponent } from './post/post.component';
+import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
-  declarations: [PostsComponent, PostComponent],
+  declarations: [PostsComponent, PostComponent, CreateComponent],
   imports: [
     CommonModule,
     PostsRoutingModule
