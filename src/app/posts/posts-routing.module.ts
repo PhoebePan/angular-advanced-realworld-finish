149eedb7116983daca4e6ffdeb4cf8f13de50778
黃升煌 Mike <mike@miniasp.com>
2020-08-19 17:45:38 +0800

    任務 01 步驟 13： posts-routing.module.ts 加入路由設定(設定 posts、post/:id 及 create 頁面顯示的元件)

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,3 +1,5 @@
+import { CreateComponent } from './create/create.component';
+import { PostComponent } from './post/post.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -8,7 +10,20 @@ const routes: Routes = [
     path: '',
     redirectTo: 'posts',
     pathMatch: 'full'
-  }];
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'post/:id',
+    component: PostComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
