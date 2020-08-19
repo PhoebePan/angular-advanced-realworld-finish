e87eb30cec46e1a77390d8d9dd891a4f07b55908
黃升煌 Mike <mike@miniasp.com>
2020-08-19 17:43:58 +0800

    任務 01 步驟 11：app-routing.module.ts 設定預設路由 (顯示 LayoutComponent 並載入 PostsModule)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [{ path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
