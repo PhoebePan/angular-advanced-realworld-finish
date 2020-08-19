bfee4511c1fc0fed4d5853ef3dec4aa5a3bae1b1
黃升煌 Mike <mike@miniasp.com>
2020-08-19 18:30:54 +0800

    任務 06 步驟 02：create.component.ts 內注入 FormBuilder 以及建立表單模型

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  post = this.formBuilder.group({
+    title: this.formBuilder.control(''),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.formBuilder.array([
+      this.formBuilder.control('Angular'),
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+    ]),
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
