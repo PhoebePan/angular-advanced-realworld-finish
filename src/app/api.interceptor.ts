8f78717b6fcc7f459602e57d38f06322655cb0cd
黃升煌 Mike <mike@miniasp.com>
2020-08-19 18:54:55 +0800

    任務 09 步驟 02：api.interceptor.ts 的 intercept() 內 設定替每次 HTTP 請求加上 localStorage 的 token 到 header 內

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -13,6 +13,16 @@ export class ApiInterceptor implements HttpInterceptor {
   constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const token = localStorage.getItem('token');
+    if (token) {
+      const newRequest = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(newRequest);
+    } else {
+      return next.handle(request);
+    }
   }
 }
