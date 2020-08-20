3c1af2b5994d4a754d50702002179486b7b473cb
黃升煌 Mike <mike@miniasp.com>
2020-08-20 21:51:20 +0800

    任務 08 步驟 05：login.component.ts 內透過 ActivatedRoute 的 queryParamMap 取得傳入的 redirect 查詢字串

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { LoginService } from './../login.service';
 import { UserLoginInfo } from './../interfaces/login-info';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -9,30 +9,40 @@ import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   user: UserLoginInfo = {
     email: '',
-    password: ''
+    password: '',
   };
 
-  constructor(private router: Router, private loginService: LoginService) { }
+  redirectUrl = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
-  }
-
-  login(): void {
-    this.loginService.login(this.user).pipe(
-      catchError((error: HttpErrorResponse) => {
-        alert(error.message);
-        return throwError(error);
-      })
-    ).subscribe(result => {
-      localStorage.setItem('token', result.user.token);
-      this.router.navigateByUrl('/');
+    this.route.queryParamMap.subscribe((queryParamMap) => {
+      this.redirectUrl = queryParamMap.get('redirect');
     });
   }
 
+  login(): void {
+    this.loginService
+      .login(this.user)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          alert(error.message);
+          return throwError(error);
+        })
+      )
+      .subscribe((result) => {
+        localStorage.setItem('token', result.user.token);
+        this.router.navigateByUrl('/');
+      });
+  }
 }
