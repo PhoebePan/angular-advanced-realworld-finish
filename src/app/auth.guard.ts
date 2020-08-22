0a9e32488ee3f97c1df883eb5594f3efc4f1251a
黃升煌 Mike <mike@miniasp.com>
2020-08-22 22:41:39 +0800

    步驟 03：auth.guard.ts 的 canActivate() 方法內 檢查 localStorage 內是否存在 token，不存在則跳到登入頁面

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -11,7 +11,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      this.router.parseUrl(`/login?redirect=${state.url}`);
+    }
   }
 
 }
